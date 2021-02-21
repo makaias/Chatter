019239b5738944eb99a20b46652192c521db4078
Makai Attila <sandorattilamakai@gmail.com>
2021-02-21 22:20:33 +0100

    used firebase authentication to see if a user is logged in, otherwise it brings the user to the startActivity, from there the user will be able to register/log in

--- app/src/main/java/com/chatter/chatter/MainActivity.java
+++ app/src/main/java/com/chatter/chatter/MainActivity.java
@@ -2,13 +2,32 @@ package com.chatter.chatter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        final FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+
+        if(currentUser == null){
+            final Intent startIntent = new Intent(MainActivity.this, StartActivity.class);
+            startActivity(startIntent);
+            finish();
+        }
     }
 }
