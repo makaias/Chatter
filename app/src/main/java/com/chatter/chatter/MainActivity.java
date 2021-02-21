81b335f5cba8a4b1d56f40194702a388477c0a83
Makai Attila <sandorattilamakai@gmail.com>
2021-02-21 23:55:07 +0100

    implemented basic logout

--- app/src/main/java/com/chatter/chatter/MainActivity.java
+++ app/src/main/java/com/chatter/chatter/MainActivity.java
@@ -1,9 +1,13 @@
 package com.chatter.chatter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -11,12 +15,16 @@ import com.google.firebase.auth.FirebaseUser;
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
+    private Toolbar mainActivityToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         firebaseAuth = FirebaseAuth.getInstance();
+        mainActivityToolbar = (Toolbar) findViewById(R.id.mainApplicationBar);
+        setSupportActionBar(mainActivityToolbar);
+        getSupportActionBar().setTitle("Chatter");
     }
 
     @Override
@@ -25,9 +33,30 @@ public class MainActivity extends AppCompatActivity {
         final FirebaseUser currentUser = firebaseAuth.getCurrentUser();
 
         if(currentUser == null){
-            final Intent startIntent = new Intent(MainActivity.this, StartActivity.class);
-            startActivity(startIntent);
-            finish();
+            updateUIToStartActivity();
         }
+    }
+
+    private void updateUIToStartActivity() {
+        final Intent startIntent = new Intent(MainActivity.this, StartActivity.class);
+        startActivity(startIntent);
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if(item.getItemId() == R.id.mainMenuLogoutItem){
+            firebaseAuth.getInstance().signOut();
+            updateUIToStartActivity();
+        }
+        return true;
     }
 }
