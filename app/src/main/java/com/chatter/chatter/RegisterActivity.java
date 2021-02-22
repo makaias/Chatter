341ae45daabbbb0dde7f19b729d77bd42a27ed82
Makai Attila <sandorattilamakai@gmail.com>
2021-02-22 20:08:27 +0100

    implemented user data synchronisation on registration with firebase realtime database

--- app/src/main/java/com/chatter/chatter/RegisterActivity.java
+++ app/src/main/java/com/chatter/chatter/RegisterActivity.java
@@ -13,8 +13,15 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.common.internal.Preconditions;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -51,15 +58,15 @@ public class RegisterActivity extends AppCompatActivity {
             final String email = emailInputLayout.getEditText().getText().toString();
             // TODO: password must be stored secure
             final String password = passwordInputLayout.getEditText().getText().toString();
-            if(TextUtils.isEmpty(name)){
+            if (TextUtils.isEmpty(name)) {
                 Toast.makeText(RegisterActivity.this, "Name required", Toast.LENGTH_LONG).show();
                 return;
             }
-            if(TextUtils.isEmpty(email)){
+            if (TextUtils.isEmpty(email)) {
                 Toast.makeText(RegisterActivity.this, "Email required", Toast.LENGTH_LONG).show();
                 return;
             }
-            if(TextUtils.isEmpty(password)){
+            if (TextUtils.isEmpty(password)) {
                 Toast.makeText(RegisterActivity.this, "Password required", Toast.LENGTH_LONG).show();
                 return;
             }
@@ -82,13 +89,37 @@ public class RegisterActivity extends AppCompatActivity {
 
         firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, task -> {
             if (task.isSuccessful()) {
+                storeDefaultUserDataToRealtimeDatabase(userName, email, task);
+            } else {
+                registrationProgressDialog.hide();
+                Log.w("RegisterActivity", "createUserWithEmail:failure", task.getException());
+                Toast.makeText(RegisterActivity.this, "Authentication failed", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void storeDefaultUserDataToRealtimeDatabase(@NonNull String userName, @NonNull String email, Task<AuthResult> task) {
+        final FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        Preconditions.checkNotNull(currentUser, "currentUser should not be null");
+        final String userUid = currentUser.getUid();
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference databaseReference = database.getReference()
+                .child("Users")
+                .child(userUid);
+        final HashMap<String, String> userDataMap = new HashMap<>();
+        userDataMap.put("name", userName);
+        userDataMap.put("description", "There is no description");
+        userDataMap.put("image", "default");
+        userDataMap.put("thumb_image", "default");
+        databaseReference.setValue(userDataMap).addOnCompleteListener(databaseTask -> {
+            if (databaseTask.isSuccessful()) {
                 registrationProgressDialog.dismiss();
                 Log.d("RegisterActivity", "createUserWithEmail:success (email: " + email);
                 updateUI();
             } else {
                 registrationProgressDialog.hide();
                 Log.w("RegisterActivity", "createUserWithEmail:failure", task.getException());
-                Toast.makeText(RegisterActivity.this, "Authentication failed", Toast.LENGTH_LONG).show();
+                Toast.makeText(RegisterActivity.this, "Database registration failed", Toast.LENGTH_LONG).show();
             }
         });
     }
